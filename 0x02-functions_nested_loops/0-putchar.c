6025f443c0a4d1b09b87d7881e23ef29e31c2097
Allan <mugabialan@gmail.com>
2022-09-22 03:37:05 -0700

    Low level c programming

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,21 @@
 #include "main.h"
 
-int main() {
-    printf("_putchar");
-    printf("\n");
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-    return 0;
+	return (0);
 }
